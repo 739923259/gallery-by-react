ee988ae9716d04b69f39593c1f2fb8d79184cda9
summer <739923259@qq.com>
2018-03-29 15:59:57 +0800

    第一次react提交代码

---
+++ src/components/Main.js
@@ -0,0 +1,25 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+let yeomanImage = require('../images/yeoman.png');
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div className="index">
+         <img src={yeomanImage} alt="Yeoman Generator" />
+        <div className="notice">
+          吃屎啦
+          <span>36666555333555</span>66669855599996666
+        </div>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
